a1795e2edc4162eadde23b55f24db876ea522344
Teodora Sandu <tas458@cs.bham.ac.uk>
2018-10-28 13:49:37 +0000

    fixing test

--- scripts/pre-commit.bash
+++ scripts/pre-commit.bash
@@ -5,6 +5,6 @@ npm t
 
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
- echo "Make sure the regression tests pass before committing!"
+ echo "\nMake sure the regression tests pass before committing!"
  exit 1
 fi
