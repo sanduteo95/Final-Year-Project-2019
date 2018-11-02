fc181149d0caca36deeca2ad12b8c86dc57cf9a0
Teodora Sandu <tas458@cs.bham.ac.uk>
2018-11-02 12:41:22 +0000

    organise repo

--- index.js
+++ index.js
@@ -4,20 +4,23 @@ const program = require('commander');
 const fs = require('fs');
 const path = require('path');
 
-const parser = require('./lib/parser.js');
-const interpreter = require('./lib/interpreter.js');
+const interpreterJS = require('./lib/toyJS/interpreter.js');
+const parserLambda = require('./lib/toyLambda/parser.js');
+const interpreterLambda = require('./lib/toyLambda/interpreter.js');
 const futamura = require('./lib/futamura.js');
 
-let code = '';
+let language = '';
 let fileName = '';
+let code = '';
 program
     .version('1.0.0')
     .usage('[options]')
     .description('Run the toy language with the provided options.')
-    .option('-p, --runParser', 'Run parser on program in the provided file')
-    .option('-i, --runInterpreter', 'Run interpreter on program in the provided file')
-    .option('-f, --runFutamura1', 'Apply the first Futamura projection on program in the provided file')
-    .action(function (file, options) {
+    .option('-p, --runParser [language] [file]', 'Run parser on program in the provided file')
+    .option('-i, --runInterpreter [language] [file]', 'Run interpreter on program in the provided file')
+    .option('-f, --runFutamura1 [language] [file]', 'Apply the first Futamura projection on program in the provided file')
+    .action(function (language, file, options) {
+        language = language;
         fileName = file;
         code = fs.readFileSync(path.join(__dirname, file), 'utf8');
     })
@@ -25,20 +28,34 @@ program
 
 if (program.runParser) {
     console.log('The result of parsing the given file is: ');
-    let result = parser.parse(code);
+    let result;
+    if (language === 'toyJS') {
+        result = parserJS.parse(code);
+    } else {
+        result = parserLambda.parse(code);
+    }
     console.log(JSON.stringify(result));
 }
 
 if (program.runInterpreter) {
     console.log('The result of interpret the given file is:');
-    const programParseTree = parser.parse(code);
-    let result = interpreter(programParseTree);
+    let result;
+    if (language === 'toyJS') {
+        result = interpreterJS(parserJS.parse(code));
+    } else {
+        result = interpreterLambda(parserLambda.parse(code));
+    }
     console.log(result);
 }
 
 if (program.runFutamura1) {
     console.log('The result of running the first Futamura projection on the given file is: ');
     const programParseTree = parser.parse(code);
-    let result = futamura.futamura1(programParseTree, fileName);
+    let result;
+    if (language === 'toyJS') {
+        result = futamura.futamura1(parserJS.parse(code), fileName, false);
+    } else {
+        result = futamura.futamura1(parserLambda.parse(code), fileName, true);
+    }
     console.log(JSON.stringify(result));
 }
--- test.js
+++ test.js
@@ -3,75 +3,130 @@ const fs = require('fs');
 const path = require('path');
 
 const input = './input/';
-const parser = require('./lib/parser.js');
-const interpreter = require('./lib/interpreter.js');
 const futamura = require('./lib/futamura.js');
 
-const results = {
-    '1.js': undefined,
-    '2.js': undefined,
-    '3.js': undefined,
-    '4.js': 5,
-    '5.js': 10,
-    '6.js': false,
-    '7.js': true,
-    '8.js': -155,
-    '9.js': -1,
-    '10.js': 'hey there',
-    '11.js': 1,
-    '12.js': 0,
-    '13.js': 'error',
-    '14.js': true,
-    '15.js': 2,
-    '16.js': 8,
-    '17.js': 1,
-    '18.js': ['hey', 'there'],
-    '19.js': 6,
-    '20.js': 5
-};
 
-describe('test interpret', function () {
-    it('should pass', function () {
-        fs.readdirSync(input).forEach(function (file) {
-            console.log('Testing ' + file);
-            console.log('It should return ' + results[file]);
-            const code = fs.readFileSync(path.join(__dirname, 'input/' + file), 'utf8');
-            const programParseTree = parser.parse(code);
-            if (results[file] === 'error') {
-                try {
-                    interpreter(programParseTree);
-                    expect(true).equal(false);
-                } catch (err) {
-                    expect(err.message).deep.equal(results[file]);
+describe('toyJS', function () {
+    const parserJS = require('./lib/toyJS/parser.js');
+    const interpreterJS = require('./lib/toyJS/interpreter.js');
+
+    const results = {
+        '1.js': undefined,
+        '2.js': undefined,
+        '3.js': undefined,
+        '4.js': 5,
+        '5.js': 10,
+        '6.js': false,
+        '7.js': true,
+        '8.js': -155,
+        '9.js': -1,
+        '10.js': 'hey there',
+        '11.js': 1,
+        '12.js': 0,
+        '13.js': 'error',
+        '14.js': true,
+        '15.js': 2,
+        '16.js': 8,
+        '17.js': 1,
+        '18.js': ['hey', 'there'],
+        '19.js': 6,
+        '20.js': 5
+    };
+    describe('test interpret', function () {
+        it('should pass', function () {
+            fs.readdirSync(input + '/toyJS').forEach(function (file) {
+                console.log('Testing ' + file);
+                console.log('It should return ' + results[file]);
+                const code = fs.readFileSync(path.join(__dirname, 'input/toyJS/' + file), 'utf8');
+                const programParseTree = parserJS.parse(code);
+                if (results[file] === 'error') {
+                    try {
+                        interpreterJS(programParseTree);
+                        expect(true).equal(false);
+                    } catch (err) {
+                        expect(err.message).deep.equal(results[file]);
+                    }
+                } else {
+                    let result = interpreterJS(programParseTree);
+                    expect(result).deep.equal(results[file]);
                 }
-            } else {
-                let result = interpreter(programParseTree);
-                expect(result).deep.equal(results[file]);
-            }
-        });
-    })
+            });
+        })
+    });
+    
+    describe('test first futamura projection', function () {
+        // increase timeout
+        this.timeout(6000);
+    
+        it('should pass', function () {
+            fs.readdirSync(input + '/toyJS').forEach(function (file) {
+                console.log('Testing ' + file);
+                console.log('It should return ' + results[file]);
+    
+                const code = fs.readFileSync(path.join(__dirname, 'input/toyJS/' + file), 'utf8');
+                const programParseTree = parserJS.parse(code);
+    
+                if (results[file] === 'error') {
+                    futamura.futamura1(programParseTree, file, false);
+                    expect(true).equal(true);
+                    // TODO: test error?
+                } else {
+                    let result = require(futamura.futamura1(programParseTree, file, false));
+                    expect(result).deep.equal(results[file]);
+                }
+            });
+        })
+    });
 });
 
-describe('test first futamura projection', function () {
-    // increase timeout
-    this.timeout(6000);
+describe.skip('toyLambda', function () {
 
-    it('should pass', function () {
-        fs.readdirSync(input).forEach(function (file) {
-            console.log('Testing ' + file);
-            console.log('It should return ' + results[file]);
+    const parserLambda = require('./lib/toyLambda/parser.js');
+    const interpreterLambda = require('./lib/toyLambda/interpreter.js');
 
-            const code = fs.readFileSync(path.join(__dirname, 'input/' + file), 'utf8');
-            const programParseTree = parser.parse(code);
-
-            if (results[file] === 'error') {
-                futamura.futamura1(programParseTree, file);
-                expect(true).equal(true);
-                // TODO: test error?
-            } else {
-                let result = require(futamura.futamura1(programParseTree, file));
-                expect(result).deep.equal(results[file]);
-            }
-        });
-    })
+    describe('test interpret', function () {
+        it('should pass', function () {
+            fs.readdirSync(input + '/toyLambda').forEach(function (file) {
+                console.log('Testing ' + file);
+                console.log('It should return ' + results[file]);
+                const code = fs.readFileSync(path.join(__dirname, 'input/toyLambda/' + file), 'utf8');
+                const programParseTree = parserLambda.parse(code);
+                if (results[file] === 'error') {
+                    try {
+                        interpreterLambda(programParseTree);
+                        expect(true).equal(false);
+                    } catch (err) {
+                        expect(err.message).deep.equal(results[file]);
+                    }
+                } else {
+                    let result = interpreterLambda(programParseTree);
+                    expect(result).deep.equal(results[file]);
+                }
+            });
+        })
+    });
+    
+    describe('test first futamura projection', function () {
+        // increase timeout
+        this.timeout(6000);
+    
+        it('should pass', function () {
+            fs.readdirSync(input + '/toyLambda').forEach(function (file) {
+                console.log('Testing ' + file);
+                console.log('It should return ' + results[file]);
+    
+                const code = fs.readFileSync(path.join(__dirname, 'input/toyLambda/' + file), 'utf8');
+                const programParseTree = parserLambda.parse(code);
+    
+                if (results[file] === 'error') {
+                    futamura.futamura1(programParseTree, file, true);
+                    expect(true).equal(true);
+                    // TODO: test error?
+                } else {
+                    let result = require(futamura.futamura1(programParseTree, file, true));
+                    expect(result).deep.equal(results[file]);
+                }
+            });
+        })
+    });
 });
