a8f642b3bcd25bbc682161b0357f7ce3f4bce829
Teodora Sandu <tas458@cs.bham.ac.uk>
2019-01-22 15:45:43 +0000

    improve benchmarking and folder struct

--- scripts/time.bash
+++ scripts/time.bash
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
+# Global variables
 STACK_SIZE=100000
 HEAP_SIZE=120000
-FILE="$2"
+# Default langauge and extension
 LANGUAGE="toyLambda"
 EXT="lambda"
 INPUT=1
 
-# Get the language
+# Check the numberof parameters
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
+# Assign the parameters
+MODE="$1"
+FILE="$2"
+STACK="$3"  
+
+# Get the language and extension
 if [[ $FILE == *"efsd"* ]]; then
     LANGUAGE="EFSD"
     EXT="efsd"
@@ -16,64 +28,67 @@ fi
 # Only get the real time
 TIMEFORMAT=%R
 
-if [[ "$1" = "-i" ]]; then
-    STACK="$3"  
-
-    # Get the location
-    LOCATION="input/$LANGUAGE/$FILE"
-    if [[ "$EXT" = "efsd" ]]; then
-        LOCATION="$LOCATION -g"
-    fi
-    if [[ $FILE == *"read"* ]]; then
-        TIME=$(time node --max-old-space-size=$HEAP_SIZE --stack-size=$STACK_SIZE index.js -i $LOCATION -s $STACK <<< $INPUT > /dev/null)
-    else
-        TIME=$(time node --max-old-space-size=$HEAP_SIZE --stack-size=$STACK_SIZE index.js -i $LOCATION -s $STACK > /dev/null)
-    fi
-else
-    if [[ "$1" = "-f" ]]; then
-        STACK="$3"
-
+case "$MODE" in
+    "-i")
+        # Get the location
+        LOCATION="input/$LANGUAGE/$FILE"
+        if [[ "$EXT" = "efsd" ]]; then
+            LOCATION="$LOCATION -g"
+        fi
+        if [[ $FILE == *"read"* ]]; then
+            TIME=$(time node --max-old-space-size=$HEAP_SIZE --stack-size=$STACK_SIZE index.js -i $LOCATION -s $STACK <<< $INPUT > /dev/null)
+        else
+            TIME=$(time node --max-old-space-size=$HEAP_SIZE --stack-size=$STACK_SIZE index.js -i $LOCATION -s $STACK > /dev/null)
+        fi
+        ;;
+    "-f")
         # Get the location
         NEW_EXT="js"
-        LOCATION="output/$LANGUAGE/${FILE/$EXT/$NEW_EXT}"
-        $(node --max-old-space-size=120000 --stack-size=${stackSize} index.js -f input/$LANGUAGE/$FILE -s ${maxTermCall} > /dev/null)
-        if [[ $FILE == *"read"* ]]; then
-            TIME=$(time node --max-old-space-size=$HEAP_SIZE --stack-size=$STACK_SIZE $LOCATION <<< $INPUT > /dev/null)
-        else
-            TIME=$(time node --max-old-space-size=$HEAP_SIZE --stack-size=$STACK_SIZE $LOCATION > /dev/null)
+        LOCATION="input/$LANGUAGE/$FILE"
+        OUTPUT_LOCATION="output/$LANGUAGE/${FILE/$EXT/$NEW_EXT}"
+        if [[ "$EXT" = "efsd" ]]; then
+            LOCATION="$LOCATION -g"
         fi
-    else
-        if [[ "$1" = "-o" ]];
-        then
-            if [[ $FILE == *"lambda"* ]]; then
-                echo $"The -o mode only works on the EFSD."
-                exit 1
-            fi
-            FILE=${FILE/.efsd/}
-            echo $FILE
-            $(cd ../EFSD; sh build_example.sh ${FILE/.efsd/} -js > /dev/null)
-         
-            case "$FILE" in
-                "incremental")
-                    INPUT="100"
-                    ;;
-                "light_control")
-                    INPUT="100"
-                    ;;
-                "cellular_automata")
-                    INPUT="10 100"
-                    ;;
-                "planets")
-                    INPUT="10 100"
-                    ;;
-                *)
-            esac
-            TIME=$(time node --max-old-space-size=$HEAP_SIZE --stack-size=$STACK_SIZE ../EFSD/example_build/js/$FILE.js $INPUT)
+        $(node --max-old-space-size=120000 --stack-size=$STACK_SIZE index.js -f $LOCATION -s $STACK > /dev/null)
+        if [[ $FILE == *"read"* ]]; then
+            TIME=$(time node --max-old-space-size=$HEAP_SIZE --stack-size=$STACK_SIZE $OUTPUT_LOCATION <<< $INPUT > /dev/null)
         else
-            echo $"There are only three modes: -i, -f, and -o."
+            TIME=$(time node --max-old-space-size=$HEAP_SIZE --stack-size=$STACK_SIZE $OUTPUT_LOCATION > /dev/null)
+        fi
+        ;;
+    "-o")
+        if [[ $FILE == *"lambda"* ]]; then
+            echo $"The -o mode only works on the EFSD."
             exit 1
         fi
-    fi
-fi
-
+        FILE=${FILE/.efsd/}
+        $(cd ../EFSD; sh build_example.sh ${FILE/.efsd/} -js > /dev/null)
+        
+        case "$FILE" in
+            "incremental")
+                INPUT="100"
+                ;;
+            "light_control")
+                INPUT="100"
+                ;;
+            "cellular_automata")
+                INPUT="10 100"
+                ;;
+            "planets")
+                INPUT="10 100"
+                ;;
+            *)
+        esac
+        TIME=$(time node --max-old-space-size=$HEAP_SIZE --stack-size=$STACK_SIZE ../EFSD/example_build/js/$FILE.js $INPUT)
+        ;;
+    "-h")
+        echo "Not implemented yet."
+        exit 1
+        ;;
+    "-a")
+        echo "Not implemented yet."
+        exit 1
+        ;;
+    *)
+esac
 echo $TIME
