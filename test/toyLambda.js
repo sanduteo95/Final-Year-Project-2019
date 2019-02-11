8242b93c962a5870692bb43e2e3ea9ec52388169
Teodora Sandu <tas458@cs.bham.ac.uk>
2019-02-11 22:50:45 +0000

    switched test names

--- test/toyLambda.js
+++ test/toyLambda.js
@@ -9,59 +9,102 @@ const boilerplate = require('../lib/boilerplate.js');
 const input = path.join(__dirname, '../input');
 const output = path.join(__dirname, '../output');
 
-describe('test', function () {
+describe('test toyLambda', function () {
+    // used to test code that expects user input
+    const testInput = 1;
+
+    // the correct results to compare against
     const results = {
-        'alt.efsd': 2,
-        'basic.efsd': 5,
-        'dfg.efsd': 4,
-        'fir.efsd': 1,
-        'link.efsd': undefined,
-        'max.efsd': 3,
-        'rsum.efsd': 21
+        'simple.lambda': 3,
+        'nested.lambda': 7,
+        'definition.lambda': 4,
+        'function.lambda': 5,
+        'addAbstr.lambda': 'function',
+        'abstr.lambda': 'function',
+        'separateArgs.lambda': 5,
+        'huge.lambda': 12,
+        'print.lambda': true,
+        'read.lambda': testInput,
+        'readAndPrint.lambda': true,
+        'recursion.lambda': 45,
+        'longRecursion.lambda': 49995000,
+        'church.lambda': 4
     };
 
     describe('test interpret', function () {
-        // increase timeout (huge because of fir)
+        // increase timeout
         this.timeout(300000);
 
-        // read all files in the EFSD folder and test them against expected results
-        fs.readdirSync(path.join(input, '/EFSD')).forEach(function (file) {
+        // read all files in the toyLambda folder and test them against expected results
+        fs.readdirSync(path.join(input, '/toyLambda')).forEach(function (file) {
             if (results[file]) {
-                it(file + ' should pass with ' + JSON.stringify(results[file]), function (done) {
-                    const code = fs.readFileSync(path.join(input, '/EFSD/' + file), 'utf8');
-                    boilerplate.interpreterBoilerplateTest(code, function (err, result) {
-                        expect(err).equal(null);
-                        expect(result).equal(results[file]);
-                        done();
-                    })(`${input}/EFSD/${file}`);
+                it(file + ' should pass with ' + results[file], function (done) {
+                    // code that expects user input needs to be tested via terminal => use Node.js exec 
+                    if (file === 'read.lambda') {
+                        exec(`node index.js -i input/toyLambda/${file} <<< ${testInput}`, function (err, stdout) {
+                            expect(err).equal(null);
+                            expect(stdout).equal('User input:\n' + testInput + '\n');
+                            done();
+                        });
+                    } else if (file === 'readAndPrint.lambda') {
+                        exec(`node index.js -i input/toyLambda/${file} <<< ${testInput}`, function (err, stdout) {
+                            expect(err).equal(null);
+                            expect(stdout).equal('User input:\n' + testInput + '\n' + results[file] + '\n');
+                            done();
+                        });
+                    } else {
+                        const code = fs.readFileSync(path.join(input, '/toyLambda/' + file), 'utf8');
+                        boilerplate.interpreterBoilerplateTest(code, function (err, result) {
+                            expect(err).equal(null);
+    
+                            if (results[file] === 'function') {
+                                expect(typeof result).equal(results[file]);
+                            } else {
+                                expect(result).deep.equal(results[file]);
+                            }
+                            done();
+                        })(`${input}/toyLambda/${file}`);
+                    }
                 });
             }
         });
     });
     
     describe('test first futamura projection', function () {
-        // increase timeout (huge because of fir)
+        // increase timeout
         this.timeout(300000);
-
-        // read all files in the EFSD folder and test them against expected resultss
-        fs.readdirSync(path.join(input, '/EFSD')).forEach(function (file) {
+    
+        // read all files in the toyLambda folder and test them against expected results
+        fs.readdirSync(path.join(input, '/toyLambda')).forEach(function (file) {
             if (results[file]) {
-                it(file + ' should pass with ' + JSON.stringify(results[file]), function (done) {
-                    const code = fs.readFileSync(path.join(input, '/EFSD/' + file), 'utf8');
-
-                    boilerplate.futamuraBoilerplate(code, 125, false)(`${input}/EFSD/${file}`)
-                        .then(futamuraResult => {
-                            if (file === 'alt.efsd' || file === 'fir.efsd' || file === 'max.efsd' || file === 'rsum.efsd' || file === 'dfg.efsd' || file === 'link.efsd') {
-                                // cannot test properly without executing the script
-                                // because it may have timeouts (contains residual code)
-                                exec(`node ${output.replace(/\ /g, '\\ ')}/EFSD/${file.substring(0, file.indexOf('.') + 1)}js`, function (err, stdout) {
+                it(file + ' should pass with ' + results[file], function (done) {
+                    const code = fs.readFileSync(path.join(input, '/toyLambda/' + file), 'utf8');
+                    boilerplate.futamuraBoilerplate(code, 125, 0)(`${input}/toyLambda/${file}`)
+                        .then(futamuraResult => {  
+                            if (results[file] === 'function') {
+                                const result = require(futamuraResult);
+                                expect(typeof result).equal(results[file]);
+                                done();
+                            }
+                            else if (file === 'read.lambda') {
+                                // cannot test this properly without executing the script 
+                                // because it needs to wait for user input
+                                exec(`node output/toyLambda/${file.substring(0, file.indexOf('.') + 1)}js <<< ${testInput}`, function (err, stdout) {
                                     expect(err).equal(null);
-                                    expect(stdout).equal(results[file] + '\n');
+                                    expect(stdout).equal('User input:\n' + testInput + '\n');
+                                    done();
+                                });
+                            } else if (file === 'readAndPrint.lambda') {
+                                // cannot test this properly without executing the script 
+                                // because it needs to wait for user input
+                                exec(`node output/toyLambda/${file.substring(0, file.indexOf('.') + 1)}js <<< ${testInput}`, function (err, stdout) {
+                                    expect(err).equal(null);
+                                    expect(stdout).equal('User input:\n' + testInput + '\n' + testInput + '\n');
                                     done();
                                 });
                             } else {
                                 const result = require(futamuraResult);
-                                expect(result).equal(results[file]);
+                                expect(result).deep.equal(results[file]);
                                 done();
                             }
                         }).catch(err => {
